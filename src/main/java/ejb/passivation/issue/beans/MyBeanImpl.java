1e0b845342fb44fbc9f43f3a8547c92b04281257
mike.jones@filament.uk.com <mike.jones@filament.uk.com>
2018-04-04 17:57:17 +0100

    Removed experiment with stateful jaxrs

--- src/main/java/ejb/passivation/issue/beans/MyBeanImpl.java
+++ src/main/java/ejb/passivation/issue/beans/MyBeanImpl.java
@@ -1,6 +1,8 @@
 package ejb.passivation.issue.beans;
 
 import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 
 import ejb.passivation.issue.interfaces.MyBean;
 
@@ -8,17 +10,15 @@ import javax.ejb.Remote;
 import javax.ejb.SessionBean;
 import javax.ejb.SessionContext;
 
-import java.io.Serializable;
 import java.rmi.RemoteException;
 
 import javax.ejb.EJBException;
 import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
 
 @Stateful
 @Remote(MyBean.class)
 @LocalBean
-public class MyBeanImpl implements MyBean, SessionBean, Serializable {
+public class MyBeanImpl implements MyBean, SessionBean {
 
 	SessionContext ctx;
 
@@ -44,17 +44,20 @@ public class MyBeanImpl implements MyBean, SessionBean, Serializable {
 		value = x;
 	}
 
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 	@Override
 	public void ejbActivate() throws EJBException, RemoteException {
 		System.out.println("Activating bean with id: " + id);
 
 	}
 
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 	@Override
 	public void ejbPassivate() throws EJBException, RemoteException {
 		System.out.println("Passivating bean with id: " + id);
 	}
 
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 	@Override
 	public void ejbRemove() throws EJBException, RemoteException {
 		System.out.println("Removing bean with id: " + id);
